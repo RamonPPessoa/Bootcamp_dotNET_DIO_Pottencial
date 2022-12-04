c47b72c8f064fc784d0e4d872d9fbce22959fe13
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-04 07:33:38 -0400

    Configurando o metodo da controller

--- FrontEndMVC/Controllers/ContatoController.cs
+++ FrontEndMVC/Controllers/ContatoController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FrontEndMVC.Context;
 
 namespace FrontEndMVC.Controllers
 {
     public class ContatoController : Controller
     {
+
+        private readonly AgendaContext _context;
+        
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
