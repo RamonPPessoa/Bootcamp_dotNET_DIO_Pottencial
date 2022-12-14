99177744d9d70f84ae8007de7798c1e3c4921d10
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-14 11:12:44 -0400

    Criando a página de Editar

--- FrontEndMVC/Controllers/ContatoController.cs
+++ FrontEndMVC/Controllers/ContatoController.cs
@@ -39,5 +39,15 @@ namespace FrontEndMVC.Controllers
             }
             return View(contato);
         }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if(contato --null)
+            return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
     }
 }
