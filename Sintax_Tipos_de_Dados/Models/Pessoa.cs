87edb151b7b0473657bd1179aedaf2ccffa5a0f2
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-22 16:50:55 -0400

    Sintax e tipos de dados em C#

---
+++ Sintax_Tipos_de_Dados/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sintax_Tipos_de_Dados.Models
+{
+    public class Pessoa
+    {
+        
+    }
+}
