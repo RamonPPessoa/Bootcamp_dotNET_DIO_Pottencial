2367081e09c74e1adb2e9e9d9579e7fe8b552397
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-22 18:35:33 -0400

    Declarando os tipos das variáveis

--- Sintax_Tipos_de_Dados/Program.cs
+++ Sintax_Tipos_de_Dados/Program.cs
@@ -1,8 +1,25 @@
 ﻿using Sintax_Tipos_de_Dados.Models;
  
 
-Pessoa pessoa01 = new Pessoa();
+string apresentacao = "Bootcamp .NET";
 
-pessoa01.Nome = "Alex";
-pessoa01.Idade = 28;
-pessoa01.Apresentar();
+int quantidade = 1;
+double altura = 1.70;
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+
+Console.WriteLine(apresentacao);
+System.Console.WriteLine("Valor da quantidade: " + quantidade);
+System.Console.WriteLine("Valor da altura: " + altura.ToString("0.00"));
+System.Console.WriteLine("Valor do preço: " + preco);
+System.Console.WriteLine("A condição é: " + condicao);
+
+
+
+// Pessoa pessoa01 = new Pessoa();
+
+// pessoa01.Nome = "Alex";
+// pessoa01.Idade = 28;
+// pessoa01.Apresentar();
