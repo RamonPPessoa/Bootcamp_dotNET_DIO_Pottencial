c3f2daeee64f9963c4ad91a595477fcfa133d9a5
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-26 18:20:01 -0400

    Diferenças entre Convert e Parse

--- TiposOperadores/Program.cs
+++ TiposOperadores/Program.cs
@@ -1,9 +1,23 @@
-﻿int n1 = 10;
-int n2 = 5;
+﻿// Cast - casting
 
-int result = n1 + n2;
+int a = Convert.ToInt32(null); // Mais apropriado usar o Convert pois o mesmo aceita valores nulos (null)
 
- result +=5;
+//Casting convertendo tipos de variáveis
 
- Console.WriteLine(result);
+
+
+ //int a = int.Parse("5"); // o Parse não aceita null
+  Console.WriteLine(a);
+
+
+
+
+// int n1 = 10;
+// int n2 = 5;
+
+// int result = n1 + n2;
+
+//  result +=5;
+
+//  Console.WriteLine(result);
 
