04ecb0b6cc24a4759236901683ec201edd046700
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-03 19:09:23 -0400

    Utilizando estruturas de repetição: while

--- EstruturasRepeticao/Program.cs
+++ EstruturasRepeticao/Program.cs
@@ -1,5 +1,13 @@
-﻿int numero = 5;
+﻿int numero = 4;
+int i =0;
+while (i <=10)
+{
+    System.Console.WriteLine($"{i}º Execução: {numero} x {i} ={numero * i}");
+    i++;
+}
+
+/*int numero = 5;
 for (int i = 0; i<= 10;i++)
 {
     System.Console.WriteLine($"{numero} x {i} ={numero * i}");
-}
+}*/
