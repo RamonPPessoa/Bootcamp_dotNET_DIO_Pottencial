c604edc814568adf64d64974183d7dcadfd8f10c
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-04 10:36:25 -0400

    Impementação de array

--- ArrayEListas/Program.cs
+++ ArrayEListas/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int[] arrayInteiro = new int[3];
+
+arrayInteiro[0] = 10;
+arrayInteiro[1] = 20;
+arrayInteiro[2] = 30;
+
+for (int i =  0; i < arrayInteiro.Length; i++) 
+{
+    System.Console.WriteLine($"Posicao Nº: {i} = {arrayInteiro[i]}");
+}
