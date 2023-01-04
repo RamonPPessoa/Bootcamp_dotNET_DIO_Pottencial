afceebf9995dedd406c7629841267d2145e9d572
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-04 09:05:24 -0400

    Criação de um menu

--- EstruturasRepeticao/Program.cs
+++ EstruturasRepeticao/Program.cs
@@ -1,12 +1,60 @@
-﻿
-int soma = 0, numero =0;
+﻿Console.WriteLine("======================MENU==============================");
 
-do
-{
-    System.Console.WriteLine("Digite um numero e (0 para parar");
-    numero = Convert.ToInt32(Console.ReadLine());
-} while (numero!=0);
-System.Console.WriteLine($"O total da soma dos numeros e: {soma}");
+ string opcao;
+
+while (true)
+ {
+     Console.Clear();
+    Console.WriteLine("Escolha uma opção");
+    Console.WriteLine("Cadastrar");
+    Console.WriteLine("Buscar");
+     Console.WriteLine("Apagar");
+    Console.WriteLine("Encerrar");
+
+     opcao = Console.ReadLine();
+     switch (opcao)
+     {
+         case "1":
+             Console.WriteLine("Cadastrar");
+             break;
+
+        case "2":
+          Console.WriteLine("Buscar");
+             break;
+
+         case "3":
+              Console.WriteLine("Apager");
+             break;
+
+         case "4":
+              Console.WriteLine("Encerrar");
+              Environment.Exit(0);
+             break;
+
+        
+         default:
+          Console.WriteLine("Opcao Inválida");
+            break;
+     }
+ }
+
+
+
+
+
+//System.Console.WriteLine("================ DO While =================================");
+
+// int soma = 0, numero =0;
+
+// do
+// {
+//     System.Console.WriteLine("Digite um numero e (0 para parar");
+//     numero = Convert.ToInt32(Console.ReadLine());
+// } while (numero!=0);
+// System.Console.WriteLine($"O total da soma dos numeros e: {soma}");
+
+//System.Console.WriteLine("================ Fim DO While =================================");
+
 /*int numero = 4;
 int i =0;
 while (i <=10)
