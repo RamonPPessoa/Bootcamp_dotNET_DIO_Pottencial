b3399b45a6e658869405f2778d2c06ba3f378b3c
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-04 11:41:34 -0400

    Redimensionando array:Array.Resize

--- ArrayEListas/Program.cs
+++ ArrayEListas/Program.cs
@@ -6,15 +6,17 @@ arrayInteiro[0] = 10;
 arrayInteiro[1] = 20;
 arrayInteiro[2] = 30;
 
+
+Array.Resize(ref arrayInteiro,arrayInteiro.Length * 2);
 for (int i =  0; i < arrayInteiro.Length; i++) 
 {
     System.Console.WriteLine($"Posicao Nº: {i} = {arrayInteiro[i]}");
 }
 
-System.Console.WriteLine("Utilizando o foreach");
-int countForeach = 0;
-foreach (var item in arrayInteiro)
-{
-    System.Console.WriteLine($"Posição Nº{countForeach} =  {item}");
-    countForeach++;
-}
+// System.Console.WriteLine("Utilizando o foreach");
+// int countForeach = 0;
+// foreach (var item in arrayInteiro)
+// {
+//     System.Console.WriteLine($"Posição Nº{countForeach} =  {item}");
+//     countForeach++;
+// }
