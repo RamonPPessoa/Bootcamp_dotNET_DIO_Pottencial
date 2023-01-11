eb4d15b18fbb8b023cb29f5b4ef986e996568d93
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-11 18:29:11 -0400

    Propriedades,métodos e construtores

--- ArrayEListas/Program.cs
+++ ArrayEListas/Program.cs
@@ -1,19 +1,41 @@
 ﻿
 
-int[] arrayInteiro = new int[3];
 
-arrayInteiro[0] = 10;
-arrayInteiro[1] = 20;
-arrayInteiro[2] = 30;
+List<string> listaDeString = new List<string>();
+
+listaDeString.Add("AM");
+listaDeString.Add("AC");
+listaDeString.Add("AP");
 
 
-int[] arrayInteiroDobro = new int[arrayInteiro.Length * 2];
-
-//Array.Resize(ref arrayInteiro,arrayInteiro.Length * 2);
-for (int i =  0; i < arrayInteiro.Length; i++) 
+for (int i = 0; i <   listaDeString.Count; i++)
 {
-    System.Console.WriteLine($"Posicao Nº: {i} = {arrayInteiro[i]}");
+    Console.WriteLine($"{i} - {listaDeString[i]}");
 }
+
+Console.WriteLine("***********LISTA COM FOREACH********************");
+int contadorForeach = 0;
+foreach(string item in listaDeString)
+{
+    Console.WriteLine($"{contadorForeach} - {item}");
+    contadorForeach++;
+}
+
+
+// int[] arrayInteiro = new int[3];
+
+// arrayInteiro[0] = 10;
+// arrayInteiro[1] = 20;
+// arrayInteiro[2] = 30;
+
+
+// int[] arrayInteiroDobro = new int[arrayInteiro.Length * 2];
+
+// //Array.Resize(ref arrayInteiro,arrayInteiro.Length * 2);
+// for (int i =  0; i < arrayInteiro.Length; i++) 
+// {
+//     System.Console.WriteLine($"Posicao Nº: {i} = {arrayInteiro[i]}");
+// }
 
 // System.Console.WriteLine("Utilizando o foreach");
 // int countForeach = 0;
---
+++ Propriedades_Metodos_Construtores/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Propriedades_Metodos_Construtores.Models
+{
+    public class Pessoa
+    {
+         public string Nome{get;set;}
+         public int Idade { get; set; }
+
+    }
+}
