365eb8917784d6830fd6a2372c9d986dcf581d51
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-01-18 17:10:55 -0400

    Implementando metodo para listar alunos

--- Propriedades_Metodos_Construtores/Models/Curso.cs
+++ Propriedades_Metodos_Construtores/Models/Curso.cs
@@ -25,5 +25,14 @@ namespace Propriedades_Metodos_Construtores.Models
         {
             return Alunos.Remove(aluno);
         }
+
+        public void ListarAlunos()
+        {
+            System.Console.WriteLine($"Alunos do curso de:  {Nome}");
+            foreach (Pessoa aluno  in Alunos)
+            {
+                System.Console.WriteLine(aluno.NomeCompleto);
+            }
+        }
     }
 }
--- Propriedades_Metodos_Construtores/Program.cs
+++ Propriedades_Metodos_Construtores/Program.cs
@@ -4,5 +4,31 @@
 Pessoa pessoa1 = new Pessoa();
 pessoa1.Nome = "João";
 pessoa1.Sobrenome = "Silva"; 
-pessoa1.Idade = 25;
-pessoa1.Apresentar();
+
+
+
+
+
+Pessoa pessoa2 = new Pessoa();
+pessoa2.Nome = "Joaquim";
+pessoa2.Sobrenome = "Nunes"; 
+
+
+
+
+
+Curso cursoIdiomas = new Curso();
+cursoIdiomas.Nome = "Inglês";
+cursoIdiomas.Alunos = new List<Pessoa>();
+
+
+cursoIdiomas.AdicionarAluno(pessoa1);
+cursoIdiomas.AdicionarAluno(pessoa2);
+cursoIdiomas.ListarAlunos();
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "João";
+// pessoa1.Sobrenome = "Silva"; 
+// pessoa1.Idade = 25;
+// pessoa1.Apresentar();
